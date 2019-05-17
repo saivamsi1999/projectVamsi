e0645b958128f628b99b0046d181be0ef549a52b
saivamsi1999 <saivamsi1999@gmail.com>
2019-05-17 14:09:11 +0530

    dapplogix

---
+++ dapplogix/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB_MOI7C1-k1KY1wlvb45-Dw7NWsvjLJMU",
+    authDomain: "final-17c5f.firebaseapp.com",
+    databaseURL: "https://final-17c5f.firebaseio.com",
+    projectId: "final-17c5f",
+    storageBucket: "final-17c5f.appspot.com",
+    messagingSenderId: "939386965605",
+    appId: "1:939386965605:web:232aec4892bd5261"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
